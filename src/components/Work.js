f42d9b93ca48be0f8ef126436d5e4723a6f6b129
Adrian Strozzi <adrianstrozzi@gmail.com>
2021-09-03 21:19:03 -0500

    Testing changes

--- src/components/Work.js
+++ src/components/Work.js
@@ -3,12 +3,12 @@ import Button from 'react-bootstrap/Button'
 
 const Work = () => {
   return (
-    <Card style={{ width: '18rem' }} bg="primary" text='light'>
+    <Card style={{ width: '18rem' }} bg="dark" text='light'>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
         <Card.Title>Work</Card.Title>
         <Card.Text>
-          Text
+          Hola soy Dana
         </Card.Text>
         <Button variant="primary">Study</Button>
       </Card.Body>
